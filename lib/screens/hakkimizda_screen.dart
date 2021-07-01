cb745237d2dc6989cee58a40e8c38fff069f82d6
Tolga Başkesen <tolgabaskesen@hotmail.com>
2021-07-02 00:12:35 +0300

    OK

--- lib/screens/hakkimizda_screen.dart
+++ lib/screens/hakkimizda_screen.dart
@@ -74,7 +74,7 @@ class _HakkimizdaScreenState extends State<HakkimizdaScreen> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        physics: NeverScrollableScrollPhysics(),
+        //physics: NeverScrollableScrollPhysics(), // SİL ŞURAYI SOKUK NEVER SCROLLABLE = ASLA KAYDIRILAMAZ
         child: Column(
           children: [
             Padding(
@@ -141,7 +141,7 @@ class _HakkimizdaScreenState extends State<HakkimizdaScreen> {
                   "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."),
             ),
             SizedBox(
-              height: size.height * 0.1,
+              height: size.height * 0.15,
             ),
           ],
         ),
